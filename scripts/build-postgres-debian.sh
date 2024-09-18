5621ea89aa225d78d8b4be511c298bad7da307bc
xyztony <awshaw@pm.me>
2024-09-18 22:24:30 +0200

    is this where psql is?
    Signed-off-by: xyztony <awshaw@pm.me>

--- scripts/build-postgres-debian.sh
+++ scripts/build-postgres-debian.sh
@@ -145,7 +145,7 @@ $DOCKER_OPTS $IMG_NAME /bin/bash -ex -c 'echo "Starting building postgres binari
     && cp /lib/*/libssl.so.1* /lib/*/libcrypto.so.1* ./lib || cp /usr/lib/*/libssl.so.1* /usr/lib/*/libcrypto.so.1* ./lib \
     && if [ "$ICU_ENABLED" = true ]; then cp --no-dereference /usr/lib/*/libicudata.so* /usr/lib/*/libicuuc.so* /usr/lib/*/libicui18n.so* ./lib; fi \
     && if [ -n "$POSTGIS_VERSION" ]; then cp --no-dereference /lib/*/libjson-c.so* /usr/lib/*/libsqlite3.so* ./lib ; fi \
-    && find ./bin -type f \( -name "initdb" -o -name "pg_ctl" -o -name "postgres" \) -print0 | xargs -0 -n1 patchelf --set-rpath "\$ORIGIN/../lib" \
+    && find ./bin -type f \( -name "initdb" -o -name "pg_ctl" -o -name "postgres" -o -name "psql" \) -print0 | xargs -0 -n1 patchelf --set-rpath "\$ORIGIN/../lib" \
     && find ./lib -maxdepth 1 -type f -name "*.so*" -print0 | xargs -0 -n1 patchelf --set-rpath "\$ORIGIN" \
     && find ./lib/postgresql -maxdepth 1 -type f -name "*.so*" -print0 | xargs -0 -n1 patchelf --set-rpath "\$ORIGIN/.." \
     && tar -cJvf /usr/local/pg-dist/postgres-linux-debian.txz --hard-dereference \
@@ -153,4 +153,5 @@ $DOCKER_OPTS $IMG_NAME /bin/bash -ex -c 'echo "Starting building postgres binari
         lib \
         bin/initdb \
         bin/pg_ctl \
-        bin/postgres'
+        bin/postgres \
+        bin/psql'
